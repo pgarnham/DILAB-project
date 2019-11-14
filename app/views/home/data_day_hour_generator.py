993383d137399e91ea609e656bb5e7999e73e522
pgarnham <pgarnham@uc.cl>
2019-11-14 20:03:55 -0300

    mapa decente

--- app/views/home/data_day_hour_generator.py
+++ app/views/home/data_day_hour_generator.py
@@ -1,6 +1,8 @@
 from random import randint, random
 from numpy import random as aleatorio
 from math import log
+from math import e as euler
+print(euler)
 
 lnormal = aleatorio.lognormal
 
@@ -14,24 +16,22 @@ for k in range(1, 11):
 			for j in range(1, 101):
 				ran_1 = str(random())[0:3]
 				ran_2 = str(random())[0:3]
-				if i <=4 :
-					promedio = 4
+				if i <=2 :
+					promedio = log(4)
 					#if j == 1:
-					#	promedio = 4
+					#	promedio = euler^4
 					#else:
-					#	promedio = (4 + dict_valores[i][j-1])/2
+					#	promedio = (4 + log(dict_valores[i][j-1])/2
 				else:
-					if j <= 4:
-						promedio = 4
-						#promedio = (dict_valores[i-1][j] + 4)/2
+					if j <= 2:
+						promedio = log(4)
+						#promedio = (log(dict_valores[i-1][j] + 4)/2
 					else:
-						promedio = (dict_valores[i-1][j] + dict_valores[i-2][j] + 
-							dict_valores[i-3][j] + dict_valores[i-4][j] + 
-							dict_valores[i][j-1] + dict_valores[i-1][j-1] + 
-							dict_valores[i-2][j-1] + dict_valores[i-3][j-1] + 
-							dict_valores[i][j-2] + dict_valores[i-1][j-2] + 
-							dict_valores[i-2][j-2] + dict_valores[i][j-3] + 
-							dict_valores[i-1][j-3] + dict_valores[i][j-4])/14
+						promedio = (log(dict_valores[i-1][j]) + log(dict_valores[i-2][j]) 
+							 + log(dict_valores[i][j-1]) + log(dict_valores[i-1][j-1]) 
+							 + log(dict_valores[i-2][j-1]) + log(dict_valores[i][j-2]) 
+							 + log(dict_valores[i-1][j-2]) + log(dict_valores[i-2][j-2])
+							)/8
 				'''
 				aux = 1
 				while aux == 1:
@@ -42,12 +42,15 @@ for k in range(1, 11):
 						valor = 4 
 						aux = 0
 				'''
-				print('promedio:', promedio)
+				#print('promedio:', promedio)
 				valor = lnormal(mean=promedio, sigma=1)
-				print('valor:', valor)
+				#print('valor:', valor)
 				
 				dict_valores[i][j] = valor
-				text = f"{i},{j},{str(valor)[0:3]},{ran_1},{ran_2}\n"
+				j_ = j-(i-1)
+				if j_ < 1:
+					j_ += 100
+				text = f"{i},{j_},{str(valor)},{ran_1},{ran_2}\n"
 				file.write(text)
 
 
